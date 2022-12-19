ce31c98d9efd16072a093c16466ebc190dc35b55
GuiBibeau <g.bibeaulaviolette@gmail.com>
2022-12-19 11:38:55 -0500

    explainer 101

--- metamask-with/metamask-sdk-nextjs-pages-directory/pages/index.tsx
+++ metamask-with/metamask-sdk-nextjs-pages-directory/pages/index.tsx
@@ -1,3 +1,4 @@
+import { ExplainerLayout } from '../components/Layout'
 import { useListen } from '../hooks/useListen'
 import { useMetaMask } from '../hooks/useMetaMask'
 import styles from '../styles/Home.module.css'
@@ -42,5 +43,72 @@ export default function Home() {
   const handleDisconnect = () => {
     dispatch({ type: 'disconnect' })
   }
-  return <div className={styles.container}></div>
+  return (
+    <ExplainerLayout
+      title="MetaMask SDK with Next.js"
+      caption={
+        <>
+          The <a href="https://metamask.io/sdk/">MetaMask SDK provides</a> a
+          sensible set of extra features on top of the regular{' '}
+          <a href="https://metamask.github.io/api-playground/api-documentation/">
+            MetaMask API
+          </a>
+        </>
+      }
+    >
+      <style jsx>{`
+        a {
+          text-decoration-line: underline;
+        }
+      `}</style>
+      <p>
+        MetaMask provides an API that allows developers to build applications
+        that can integrate with the MetaMask extension and interact with the
+        Ethereum blockchain. While the browser is quite popular, it is a bit
+        more difficult to use with the MetaMask mobile app. The MetaMask SDK
+        provides a sensible set of extra features on top of the regular MetaMask
+        API.
+      </p>
+
+      <h2>Installation</h2>
+      <p>
+        The MetaMask SDK is available as an npm package. You can install it with
+        npm
+      </p>
+      <pre>
+        <code>
+          npm i @metamask/sdk
+          <br />
+          // or with yarn
+          <br />
+          yarn add @metamask/sdk
+        </code>
+      </pre>
+      <p>
+        Once installed, you can build your application like you normally would
+        while using the <code>window.ethereum</code> provider. You will simply
+        need to import and instanciate the SDK like shown at{' '}
+        <code>lib/metamaskSDK.ts</code>:
+        <pre>
+          <code>
+            {`
+import MetaMaskSDK from "@metamask/sdk";
+
+export const instantiateSdk = () => {
+  if (typeof window === undefined) {
+    return null;
+  }
+
+  new MetaMaskSDK();
+};`}
+          </code>
+        </pre>
+        This will augment the <code>window.ethereum</code> provider with the SDK
+        and allow you to use the SDK API to interact with the MetaMask mobile
+        app. If users are using the MetaMask extension, the SDK will simply use
+        the regular API. Otherwise users will see a prompt to connect with a QR
+        code to the MetaMask mobile app.
+      </p>
+    </ExplainerLayout>
+  )
 }
